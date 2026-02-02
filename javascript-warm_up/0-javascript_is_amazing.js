0dbfd7948455b858e2d5dcd81d0d7d4a3b22de91
Ulfat Naiemi <u.naiemi@alustudent.com>
2026-02-02 20:51:06 +0200

    Fix merge conflict in 0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-=======
+ HEAD
 // Task 0: Print "JavaScript is amazing" using a constant
-
->>>>>>> 4cc98ec1f1690a2dd19c39034857f2941aa9ad30
+ 4cc98ec1f1690a2dd19c39034857f2941aa9ad30
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
